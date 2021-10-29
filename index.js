f17103b8c125e34b3ff1476333b0319b011ecfd3
simo-tuni <simo.hakkinen@tuni,fi>
2021-10-29 18:44:11 +0300

    1st test

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.static("public"));
+
+//app.get("/", (req, res) => {
+//  res.send("Hello World!");
+//});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
