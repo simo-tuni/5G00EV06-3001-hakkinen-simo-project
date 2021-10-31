d5e33d1e322d159ec263efc1ac8ebeaea9f8d499
simo-tuni <simo.hakkinen@tuni,fi>
2021-10-31 15:35:43 +0200

    changed router back to BrowserRouter and made api

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+const axios = require("axios");
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-//app.get("/", (req, res) => {
-//  res.send("Hello World!");
-//});
+app.get("/api/getCurrency", (req, res) => {
+  async function fetchData() {
+    const result = await axios.get(
+      `https://poe.ninja/api/data/CurrencyOverview?league=Scourge&type=Currency`
+    );
+    res.send(result.data.lines);
+  }
+  fetchData();
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
