21f90af6b67e2723d89d2cf4a879824af8529f5e
simo-tuni <simo.hakkinen@tuni,fi>
2021-11-24 20:50:11 +0200

    training request timeout fix #3

--- index.js
+++ index.js
@@ -97,10 +97,10 @@ app.get("/api/getCurrenctData", (req, res) => {
 */
 app.post("/api/createNewModel", (req, res) => {
   req.setTimeout(0, function () {
-    console.log("req.timeout");
+    return res.send("req.timeout");
   }); // Timeout is set to never time out the request, since it will take multiple minutes to train even a small neural network.
   res.setTimeout(0, function () {
-    console.log("res.timeout");
+    return res.send("res.timeout");
   });
   // Cut request into parts for clearer code
   let epochs = req.body.fitsettings.epochs;
